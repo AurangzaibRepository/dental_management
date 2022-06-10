4dccae141bfc37227f6639de668be536b9edc294
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2022-06-11 00:19:49 +0500

    Creates left menu component

---
+++ resources/js/components/layouts/LeftMenuComponent.vue
@@ -0,0 +1,27 @@
+<template>
+    <div class="dv-leftmenu">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <router-link to="/" class="nav-link">Dashboard</router-link>
+                </li>
+                <li class="nav-item">
+                    <router-link to="/dentists" class="nav-link">Dentists</router-link>
+                </li>
+                <li class="nav-item">
+                    <router-link to="/tachnicians" class="nav-link">Techicians</router-link>
+                </li>
+                <li class="nav-item">
+                    <router-link to="/patients" class="nav-link">Patients</router-link>
+                </li>
+                <li class="nav-item">
+                    <router-link to="/cases" class="nav-link">Cases</router-link>
+                </li>
+                <li class="nav-item">
+                    <router-link to="/settings" class="nav-link">Settings</router-link>
+                </li>
+                <li class="nav-item">
+                    <router-link to="/logout" class="nav-link">Logout</router-link>
+                </li>
+            </ul>
+    </div>
+</template>
