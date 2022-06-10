5d395af36af922a9c29837bf62bf3d1d8bcea8b0
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2022-06-11 00:20:39 +0500

    Imports left menu component

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -4,37 +4,23 @@
             <span class="spn-app-title">DENTALINK</span>
         </div>
         <div class="dv-layout">
-            <div class="dv-leftmenu">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <router-link to="/" class="nav-link">Dashboard</router-link>
-                    </li>
-                    <li class="nav-item">
-                        <router-link to="/dentists" class="nav-link">Dentists</router-link>
-                    </li>
-                    <li class="nav-item">
-                        <router-link to="/tachnicians" class="nav-link">Techicians</router-link>
-                    </li>
-                    <li class="nav-item">
-                        <router-link to="/patients" class="nav-link">Patients</router-link>
-                    </li>
-                    <li class="nav-item">
-                        <router-link to="/cases" class="nav-link">Cases</router-link>
-                    </li>
-                    <li class="nav-item">
-                        <router-link to="/settings" class="nav-link">Settings</router-link>
-                    </li>
-                    <li class="nav-item">
-                        <router-link to="/logout" class="nav-link">Logout</router-link>
-                    </li>
-                </ul>
-            </div>
+            <LeftMenuComponent />
             <div class="dv-content">
                 <router-view></router-view>
             </div>
         </div>    
     </div>
 </template>
+
+<script>
+import LeftMenuComponent from './components/layouts/LeftMenuComponent';
+
+export default {
+    components: {
+        LeftMenuComponent
+    }   
+}
+</script>
 
 <style scoped>
 
