b4fe96519d764ca8d065960386400d4c6d62fa86
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2022-06-13 23:20:31 +0500

    ADds add new button

--- resources/js/components/dentist/ListComponent.vue
+++ resources/js/components/dentist/ListComponent.vue
@@ -1,14 +1,11 @@
 <template>
     <div>
         <label class="page-title">Dentists</label>
-
-      
+        <div class="row">
+            <div class="col-md-12 d-flex justify-content-end">
+                <router-link to="/dentists-add" class="btn btn-primary">Add New</router-link>
+            </div>
+        </div>
+        
     </div>
 </template>
-
-<script>
-
-export default {
-    
-}
-</script>
