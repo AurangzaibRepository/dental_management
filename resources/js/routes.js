91f9346be48be3d9e8c67f0493476486fddc5464
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2022-06-13 19:55:22 +0500

    Includes denisr in route file

--- resources/js/routes.js
+++ resources/js/routes.js
@@ -1,4 +1,5 @@
 import DashboardComponent from './components/DashboardComponent';
+import DentistComponent from './components/dentist/ListComponent';
 import ProductsComponent from './components/ProductsComponent';
 import CreateProduct from './components/CreateProductComponent';
 import UpdateProduct from './components/UpdateProductComponent';
@@ -8,6 +9,11 @@ const routes = [
         name: 'dashboard',
         path: '/',
         component: DashboardComponent
+    },
+    {
+        name: 'dentists',
+        path: '/dentists',
+        component: DentistComponent
     },
     {
         name: 'products',
