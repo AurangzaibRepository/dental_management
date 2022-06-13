9f07845d012e340a7d9691ed28d9ed9899db846a
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2022-06-13 22:01:21 +0500

    Imports datatble

--- resources/js/components/dentist/ListComponent.vue
+++ resources/js/components/dentist/ListComponent.vue
@@ -1,3 +1,23 @@
 <template>
     <label class="page-title">Dentists</label>
+
+    
 </template>
+
+<script>
+import JlDatatble from 'jl-datatable';
+
+export default {
+    component: {
+        JlDatatble
+    },
+
+    data() {
+        return {
+            datatable: {
+                url: '/api/dentists/listing'
+            }
+        }
+    }
+}
+</script>
