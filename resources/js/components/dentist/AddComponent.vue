38b2c7f763a8e79540048646e64e04857052f9aa
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2022-06-14 00:15:41 +0500

    Adds fields

--- resources/js/components/dentist/AddComponent.vue
+++ resources/js/components/dentist/AddComponent.vue
@@ -2,7 +2,38 @@
     <div>
         <label class="page-title">Dentists - Add</label>
         <div class="dv-section">
-            
+            <div class="row">
+                <div class="col-md-6 mb-3">
+                    <label>First Name</label>
+                    <input type="text" class="form-control"></input>
+                </div>
+                <div class="col-md-6 mb-3">
+                    <label>Last Name</label>
+                    <input type="text"  class="form-control"></input>
+                </div>
+                <div class="col-md-6 mb-3">
+                    <label>Email</label>
+                    <input type="text" class="form-control">
+                </div>
+                <div class="col-md-6 mb-3">
+                    <label>Subscription</label>
+                    <select class="form-select">
+                        <option value="Free">Free</option>
+                        <option value="Premium">Premium</option>
+                    </select>
+                </div>
+                <div class="col-md-6 mb-3">
+                    <label>Status</label>
+                    <select class="form-select">
+                        <option value="1">Active</option>
+                        <option value="0">Inactive</option>
+                    </select>
+                </div>
+                <div class="col-md-6 mb-3">
+                    <label>Password</label>
+                    <input type="password" class="form-control"></input>
+                </div>
+            </div>
         </div>
     </div>
 </template
