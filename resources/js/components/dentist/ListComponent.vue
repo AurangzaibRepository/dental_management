1a6f6c4f3dcb6b3bc976dc9f3137d68b6c26f37a
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2022-06-13 23:38:32 +0500

    Fixes add dentist link

--- resources/js/components/dentist/ListComponent.vue
+++ resources/js/components/dentist/ListComponent.vue
@@ -3,7 +3,7 @@
         <label class="page-title">Dentists</label>
         <div class="row">
             <div class="col-md-12 d-flex justify-content-end">
-                <router-link to="/dentists-add" class="btn btn-primary">Add New</router-link>
+                <router-link to="/dentists/add" class="btn btn-primary">Add New</router-link>
             </div>
         </div>
         
