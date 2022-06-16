9627eea0567358bd6b7404d91baa6b4d8cb2c3ac
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2022-06-16 20:43:10 +0500

    Retrieves success mesage from local storage

--- resources/js/components/dentist/ListComponent.vue
+++ resources/js/components/dentist/ListComponent.vue
@@ -1,6 +1,10 @@
 <template>
     <div>
         <label class="page-title">Dentists</label>
+        <div class="alert alert-success" v-if="success">
+            Profile saved successfully
+        </div>
+
         <div class="row">
             <div class="col-md-12 d-flex justify-content-end">
                 <router-link to="/dentists/add" class="btn btn-primary">Add New</router-link>
@@ -9,3 +13,18 @@
         
     </div>
 </template>
+
+<script>
+export default {
+    data() {
+        return {
+            success: '',
+        }
+    },
+
+    mounted() {
+        this.success = localStorage.getItem('success');
+        localStorage.removeItem('success');
+    }
+}
+</script>
