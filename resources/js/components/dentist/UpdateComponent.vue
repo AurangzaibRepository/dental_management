ff894d12161ddc7b7d6ed11e20cebfd4d286628b
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2022-06-16 22:22:46 +0500

    Adds form fields

--- resources/js/components/dentist/UpdateComponent.vue
+++ resources/js/components/dentist/UpdateComponent.vue
@@ -1,5 +1,38 @@
 <template>
     <div>
         <label class="page-title">Dentists - Update</label>
+
+        <div class="dv-section">
+            <form>
+
+                <div class="row">
+                    <div class="col-md-6 mb-3">
+                        <label>First Name</label>
+                        <input type="text" class="form-control"></input>
+                    </div>
+                    <div class="col-md-6 mb-3">
+                        <label>Last Name</label>
+                        <input type="text" class="form-control"></input>
+                    </div>
+                    <div class="col-md-6 mb-3">
+                        <label>Email</label>
+                        <input type="text" class="form-control"></input>
+                    </div>
+                    <div class="col-md-6 mb-3">
+                        <label>Subscription</label>
+                        <select class="form-select"></select>
+                    </div>
+                    <div class="col-md-6 mb-3">
+                        <label>Status</label>
+                        <select class="form-select"></select>
+                    </div>
+                    <div class="col-md-6 mb-3">
+                        <label>Password</label>
+                        <input type="password" class="form-control"></input>
+                    </div>
+                    
+                </div>
+            </form>
+        </div>
     </div>
 </template>
