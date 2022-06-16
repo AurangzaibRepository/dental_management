f13e5aaa90d610cdcc77726977ff8f30b92fe6ab
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2022-06-16 20:54:26 +0500

    Assigns defaulr valeu for props

--- resources/js/components/partials/ErrorsComponent.vue
+++ resources/js/components/partials/ErrorsComponent.vue
@@ -1,15 +1,30 @@
 <template>
-    <div v-if="errors.length" class="alert alert-danger">
-        <ul>
-            <li v-for="error in errors">{{error}}</li>
-        </ul>
+    <div>
+        <div v-if="success" class="alert alert-danger">
+            {{success}}
+        </div>
+
+        <div v-if="errors.length" class="alert alert-danger">
+            <ul>
+                <li v-for="error in errors">{{error}}</li>
+            </ul>
+        </div>
     </div>
 </template>
 
 <script>
 export default {
     props: {
-        errors: Array
+        errors: {
+            type: Array,
+            default: function () {
+                return []
+            }
+        },
+        success: {
+            type: String,
+            default: ''
+        }
     }
 }
 </script>
