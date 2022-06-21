e9f27f9ce908a39547a5345d92f4dbc38041b5ae
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2022-06-21 23:22:49 +0500

    Includes registration request

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -5,10 +5,11 @@ namespace App\Http\Controllers\API;
 use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
 use App\Http\Controllers\Controller;
+use App\Http\Requests\RegisterRequest;
 
 class AuthController extends Controller
 {
-    public function register(Request $request): JsonResponse
+    public function register(RegisterRequest $registerRequest): JsonResponse
     {
         return response()->json([
             'status' => true,
