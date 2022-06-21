e389fbae41e714e0266549937c2b77619ee30720
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2022-06-21 22:57:05 +0500

    Implements response function

--- app/Http/Requests/RegisterRequest.php
+++ app/Http/Requests/RegisterRequest.php
@@ -3,14 +3,24 @@
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Contracts\Validation\Validator;
+use Illuminate\Http\Exceptions\HttpResponseException;
 
 class RegisterRequest extends FormRequest
 {
-    public function rules()
+    public function rules(): array
     {
         return [
             'first_name' => 'required',
             'last_name' => 'required'
         ];
+    }
+
+    public function failedValidation(Validator $validator)
+    {
+        throw new HttpResponseException(response()->json([
+            'status' => false,
+            'errors' => $validator->errors()
+        ]));
     }
 }
