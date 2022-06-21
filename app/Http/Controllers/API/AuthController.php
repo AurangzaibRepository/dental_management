7b910f25510d563cd20082faa308b748b919d1db
Aurangzaib Najam <aurangzaib_najam@hotmail.com>
2022-06-21 22:23:04 +0500

    Returns json response

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -3,11 +3,16 @@
 namespace App\Http\Controllers\API;
 
 use Illuminate\Http\Request;
+use Illuminate\Http\JsonResponse;
 use App\Http\Controllers\Controller;
 
 class AuthController extends Controller
 {
-    public function register(Request $request)
+    public function register(Request $request): JsonResponse
     {
+        return response()->json([
+            'status' => true,
+            'message' => 'Registration completed successfully'
+        ]);
     }
 }
