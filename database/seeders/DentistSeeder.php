ecfa6f7be45f5191ced6bef8fa81e2180724742e
Aurnagzaib Najam <aurangzaib_najam@hotmail.com>
2022-06-23 17:42:00 +0500

    Creates dentist seeder

---
+++ database/seeders/DentistSeeder.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DentistSeeder extends Seeder
+{
+    public function run()
+    {
+    }
+}
