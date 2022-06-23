6affb6216e07e209bdce81fb1649d40fd6c6e44b
Aurnagzaib Najam <aurangzaib_najam@hotmail.com>
2022-06-23 19:54:55 +0500

    Includes dentist mdoel

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -6,11 +6,21 @@ use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\RegisterRequest;
+use App\Models\Dentist;
 
 class AuthController extends Controller
 {
-    public function register(RegisterRequest $registerRequest): JsonResponse
+    public function __construct(
+        private Dentist $dentist
+    ) {
+    }
+
+    public function register(RegisterRequest $request): JsonResponse
     {
+        if ($request->role === 'Dentist') {
+            $this->dentist->register();
+        }
+
         return response()->json([
             'status' => true,
             'message' => 'Registration completed successfully'
