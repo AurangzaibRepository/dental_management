658bcf6c4c873e0b82b4eeb16cccd95176669733
Aurnagzaib Najam <aurangzaib_najam@hotmail.com>
2022-06-23 19:39:34 +0500

    Specifie sattrbutes ib factory

--- database/factories/DentistFactory.php
+++ database/factories/DentistFactory.php
@@ -12,7 +12,13 @@ class DentistFactory extends Factory
     public function definition(): array
     {
         return [
-            
+            'first_name' => $this->faker->name(),
+            'last_name' => $this->faker->name(),
+            'email' => $this->faker->unique()->safeEmail(),
+            'phone_number' => $this->faker->unique()->phoneNumber(),
+            'password' => $this->faker->password(),
+            'subscription' => $this->faker->randomElement(['Free', 'Premium']),
+            'active' => $this->faker->randomElement(['0','1'])
         ];
     }
 }
