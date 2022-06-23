e69ecc42d993ffb7414c4d5a5abc60fe01f32bc9
Aurnagzaib Najam <aurangzaib_najam@hotmail.com>
2022-06-23 20:26:16 +0500

    Implements mutator

--- app/Models/Dentist.php
+++ app/Models/Dentist.php
@@ -3,11 +3,29 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Hash;
 
 class Dentist extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'email',
+        'phone_number',
+        'password',
+        'subscription'
+    ];
+
+    public function password(): Attribute
+    {
+        return Attribute::make(
+            set: fn ($value) => Hash::make($value) 
+        );      
+    }
 
     public function register(array $data): void
     {
