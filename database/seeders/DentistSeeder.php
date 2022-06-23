64b6e28b3e38ac0ecd9b52542a10a11610dff79d
Aurnagzaib Najam <aurangzaib_najam@hotmail.com>
2022-06-23 19:39:53 +0500

    Calls factory in seeder

--- database/seeders/DentistSeeder.php
+++ database/seeders/DentistSeeder.php
@@ -4,10 +4,14 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Dentist;
 
 class DentistSeeder extends Seeder
 {
     public function run()
     {
+        Dentist::factory()
+            ->count(5)
+            ->create();
     }
 }
